d19148fe397d85d089f1f7e41851bcff0fb61f2d
AfnanElhussini <afnanrlhussini23@gmail.com>
2023-03-26 23:06:56 +0200

    Trying images

--- src/app/_services/order.service.ts
+++ src/app/_services/order.service.ts
@@ -18,8 +18,9 @@ export class OrderService {
       //   'Bearer ' +
       //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDBmNDY2MjBlNTc4MTdiMDY4Zjk4ZTUiLCJpYXQiOjE2Nzg3Mjk1NTMsImV4cCI6MTY4NjUwNTU1M30.hF3wrJ2xL7E2JmQmwyo5oqu5U8vBR1sTMThd4_S0jt4',
       // shipping address
-      'Authorization' : 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDFlYzU5ODBhYTc2MTdmYTlmNmQyNTEiLCJpYXQiOjE2Nzk3NTY4OTgsImV4cCI6MTY4NzUzMjg5OH0.1h8ZJKE3A--rA7WLiLZzR5VZmQr-c_HROKB90oNCTPw'
+      // 'Authorization' : 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDFlYzU5ODBhYTc2MTdmYTlmNmQyNTEiLCJpYXQiOjE2Nzk3NTY4OTgsImV4cCI6MTY4NzUzMjg5OH0.1h8ZJKE3A--rA7WLiLZzR5VZmQr-c_HROKB90oNCTPw'
       // 'Authorization' : 'Bearer ' + localStorage.getItem('token')
+             'Authorization' : 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDBmNDY2MjBlNTc4MTdiMDY4Zjk4ZTUiLCJpYXQiOjE2Nzg3Mjk1NTMsImV4cCI6MTY4NjUwNTU1M30.hF3wrJ2xL7E2JmQmwyo5oqu5U8vBR1sTMThd4_S0jt4'
     }),
   };
 
